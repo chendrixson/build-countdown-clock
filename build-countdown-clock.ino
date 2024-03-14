5add517dec2112c36eba85ece2a0b03794c8ec5b
chendrixson <chendrix@microsoft.com>
2024-03-13 19:15:57 -0700

    Add support for an RTC module if you have it connected.  USE_RTC

--- build-countdown-clock.ino
+++ build-countdown-clock.ino
@@ -1,6 +1,13 @@
 #include <Wire.h>
 #include <Adafruit_SSD1306.h>
 
+#define USE_RTC
+
+#ifdef USE_RTC
+#include <RTClib.h>
+RTC_DS3231 rtc;
+#endif
+
 Adafruit_SSD1306 display(128, 64, &Wire, -1);
 const int DISPLAY_ADDRESS = 0x3c;
 
@@ -21,6 +28,8 @@ int currentYear = 2024;
 // create a constant for may 21st, 2024
 const int targetMonth = 5;
 const int targetDay = 21;
+
+char smallBuffer[24] = {0};
 
 int calculateDayOfYear(int month, int day) {
   int daysInMonth[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
@@ -53,9 +62,8 @@ void showDate(int m, int d, int y)
   display.setTextColor(WHITE,BLACK);
   display.setCursor(60,2);
   display.setTextSize(1);
-  char dateBuffer[16] = {0};
-  sprintf(dateBuffer, "%d/%d/%d", m, d, y);
-  display.println(dateBuffer);  
+  sprintf(smallBuffer, "%2d/%d/%d  ", m, d, y);
+  display.println(smallBuffer);  
 
   // and then show how many days until showDaysTillBuild  
   int currentDayOfYear = calculateDayOfYear(currentMonth, currentDay);
@@ -69,9 +77,8 @@ void showDate(int m, int d, int y)
 
   display.setTextColor(WHITE,BLACK);
   display.setCursor(0,55);
-  char dtbBuffer[24] = {0};
-  sprintf(dtbBuffer, "%d days until //BUILD", days);
-  display.println(dtbBuffer);  
+  sprintf(smallBuffer, "%3d days till //Build", days);
+  display.println(smallBuffer);  
   display.display();
 }
 
@@ -93,16 +100,15 @@ void showTime(int h, int m, int s)
     h = 12;
   }
 
-  char buffer[16] = {0};
-  sprintf(buffer, "%2d:%02d:%02d", h, m, s);
-  display.println(buffer);
+  sprintf(smallBuffer, "%2d:%02d:%02d", h, m, s);
+  display.println(smallBuffer);
   display.setTextSize(1);
   display.setCursor(108,24);
   display.println(ampm);
   display.display();
 }
 
-
+#ifndef USE_RTC
 void nextDay()
 {
   // function to advance the current date information, taking into account the days in the month,
@@ -135,6 +141,7 @@ void nextDay()
 
   showDate(currentMonth, currentDay, currentYear);
 }
+#endif
 
 void setTime(int h, int m, int s)
 {
@@ -143,56 +150,143 @@ void setTime(int h, int m, int s)
   currentTimeSecond = s;
 }
 
+void setDate(int m, int d, int y)
+{
+  currentMonth = m;
+  currentDay = d;
+  currentYear = y;
+}
+
+void outputDateAndTime(int h, int m, int s, int month, int day, int year)
+{
+  Serial.print(F("time: "));
+  char outBuffer[16] = {0};
+  sprintf(outBuffer, "%d:%d:%d", h, m, s);
+  Serial.println(outBuffer);
+  Serial.print(F("date: "));
+  sprintf(outBuffer, "%d/%d/%d", month, day, year);
+  Serial.println(outBuffer);
+}
+
+#ifdef USE_RTC
+void getRTC(bool dumpInfo = false)
+{
+  DateTime now = rtc.now();
+
+  if(dumpInfo)
+  {
+    Serial.println("Dumping RTC info");
+    outputDateAndTime(now.hour(), now.minute(), now.second(), now.month(), now.day(), now.year());
+  }
+
+  setTime(now.hour(), now.minute(), now.second());
+  setDate(now.month(), now.day(), now.year());
+}
+
+void adjustRTC(int h, int m, int s, int month, int day, int year)
+{
+  Serial.println(F("Adjusting RTC"));
+  outputDateAndTime(h, m, s, month, day, year);
+  rtc.adjust(DateTime(year, month, day, h, m, s));
+}
+#endif
+
 void setup() {
+
+  delay(2000);
+  
+  // print out the instructions for setting the date and time
+  Serial.begin(115200);
+  Serial.println(F("Use tHH:MM:SS followed by a newline to set the time"));
+  Serial.println(F("Use dMM/DD/YY followed by a newline to set the date"));
+
   display.begin(SSD1306_SWITCHCAPVCC, DISPLAY_ADDRESS);
   clearScreen();
   delay(2000);
 
-  // print out the instructions for setting the date and time
-  Serial.begin(115200);
-  Serial.println("Use tHH:MM:SS followed by a newline to set the time\n");
-  Serial.println("Use dMM/DD/YY followed by a newline to set the date\n");
+#ifdef USE_RTC
+  Serial.println(F("Initializing RTC\n"));
+  if (!rtc.begin()) {
+    Serial.println(F("RTC failed to begin!"));
+  }
+  else
+  {
+    Serial.println(F("RTC is running"));
+    DateTime now = rtc.now();
+    getRTC(true);
+    Serial.println(F("Got the RTC info"));
+  }
+#endif
 
-  setTime(0,0,0);
-
+  Serial.println(F("About to show time and date"));
   showTime(currentTimeHour, currentTimeMinute, currentTimeSecond);
   showDate(currentMonth, currentDay, currentYear);
+  Serial.println(F("Finished showing time and date"));
+  delay(2000);
 }
+
+char commandBuffer[16] = {0};
 
 void loop() {
 
   if(Serial.available() > 0)
   {
-    // read to see if the character is a T or a d
+    Serial.println(F("Got command data"));
+    // read to see if the character is a t or a d
     char c = Serial.read();
     if(c == 't')
     {
-      char buffer[16] = {0};
-      Serial.readBytesUntil('\n', buffer, 16);
-      int h, m, s;
-      if(sscanf(buffer, "%d:%d:%d", &h, &m, &s) == 3)
+      memset(commandBuffer, 0, 16);
+      Serial.readBytes(commandBuffer, 15);
+
+      int h, m, s;     
+      Serial.println(F("Got command buffer:"));
+      Serial.println(commandBuffer);
+
+      if(sscanf(commandBuffer, "%d:%d:%d", &h, &m, &s) == 3)
       {
-        setTime(h, m, s);
-        showTime(currentTimeHour, currentTimeMinute, currentTimeSecond);
+        Serial.println(F("Setting time"));        
+        #ifdef USE_RTC
+          adjustRTC(h, m, s, currentMonth, currentDay, currentYear);
+        #else
+          setTime(h, m, s);
+          showTime(currentTimeHour, currentTimeMinute, currentTimeSecond);
+        #endif
+      }
+      else
+      {
+        Serial.println(F("Failed to parse time"));
       }
     }
     else if(c == 'd')
-    {
-      Serial.println("enter the date in the format MM/DD/YY and hit enter");
-      char buffer[16] = {0};
-      Serial.readBytesUntil('\n', buffer, 16);
-      int m, d, y;
-      if(sscanf(buffer, "%d/%d/%d", &m, &d, &y) == 3)
-      {
-        currentMonth = m;
-        currentDay = d;
-        currentYear = y;
-        showDate(currentMonth, currentDay, currentYear);
-      }
-    }
+    {      
+      memset(commandBuffer, 0, 16);
+      Serial.readBytes(commandBuffer, 15);
 
+      int m, d, y;     
+      Serial.println(F("Got command buffer:"));
+      Serial.println(commandBuffer);
+
+      if(sscanf(commandBuffer, "%d/%d/%d", &m, &d, &y) == 3)
+      {
+        Serial.println(F("Setting date"));
+        #ifdef USE_RTC
+          adjustRTC(currentTimeHour, currentTimeMinute, currentTimeSecond, m, d, y);
+        #else
+          setDate(m, d, y);
+          showDate(currentMonth, currentDay, currentYear);
+        #endif
+      }
+      else
+      {
+        Serial.println(F("Failed to parse date"));
+      }      
+    }
   }
 
+#ifdef USE_RTC
+  getRTC();
+#else
   // advance current time by one second
   currentTimeSecond++;
   if(currentTimeSecond >= 60)
@@ -209,15 +303,20 @@ void loop() {
       }
     }
   }
+#endif
 
   // advance the time for each second 
   showTime(currentTimeHour, currentTimeMinute, currentTimeSecond);
 
+#ifdef USE_RTC
+  showDate(currentMonth, currentDay, currentYear);
+#else
   // if we've reached midnight, advance the date
   if(currentTimeHour == 0 && currentTimeMinute == 0 && currentTimeSecond == 0)
   {
     nextDay();
   }
+#endif
 
   delay(MILLIS_PER_SECOND);
 }
